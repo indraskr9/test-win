a661ecf917eb204aed911bbf8eb9a9dc3459ec00
Indrajit Sarkar <45512567+indraskr9@users.noreply.github.com>
2025-06-07 17:21:37 +0530

    update url

--- rusk-desk-files/rusk-desk-setup.bat
+++ rusk-desk-files/rusk-desk-setup.bat
@@ -2,7 +2,7 @@
 curl -s -L -o login.py https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/login.py
 curl -s -L -o loop.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/loop.bat
 curl -s -L -o show.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/show.bat
-certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86_64.exe" rustdesk.exe
+certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe" rustdesk.exe
 curl -s -L -o time.py https://raw.githubusercontent.com/indraskr9/test-win/refs/heads/main/rusk-desk-files/timelimit.py
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
